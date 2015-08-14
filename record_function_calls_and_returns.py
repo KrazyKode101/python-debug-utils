a3b10771f177fce3b5a5d6dd9b4db958fe13ac57
karthik <shivakarthik8@gmail.com>
2015-08-14 20:35:35 +0530

    modified record stack algo

--- record_function_calls_and_returns.py
+++ record_function_calls_and_returns.py
@@ -1,26 +1,50 @@
 import sys
+import ntpath
 
-calls = []
+calls = {}
+op_file = None
+
+def print_leaf(path):
+	head,tail = ntpath.split(path)
+	return tail or ntpath.basename(head)
 
 def record_calls(frame,event,arg):
-	if event == 'call':
-		pretty_call(frame,frame.f_locals)
-	return record_calls
-		
-def pretty_call(frame,arg):
 	func_name = frame.f_code.co_name
-	calls.append(func_name+'('+pretty_locals(arg)+')')
+	file_name = frame.f_code.co_filename
+	if event == 'call':
+		args = frame.f_locals
+		add_function_call(file_name,func_name,args)
+	elif event == 'return':
+		add_return_value(func_name,arg)
+	return record_calls
 
-def pretty_locals(arg):
+def add_function_call(file_name,func_name,args):
+	if func_name not in calls:
+		calls[func_name] = { 'module_name':'','stack_call':[], 'stack_no':-1 }
+	value = {'args':pretty_locals(args), 'ret':'' }		
+	calls[func_name]['stack_call'].append(value)
+	calls[func_name]['stack_no'] += 1
+	calls[func_name]['module_name'] = file_name
+
+def add_return_value(func_name,ret):
+	index = calls[func_name]['stack_no']
+	calls[func_name]['stack_call'][index]['ret'] = ret
+	print_calls(func_name,index)
+
+def pretty_locals(args):
 	ret = ''
-	for name,value in arg.iteritems():
+	for (name,value) in args.items():
 		if ret != '':
 			ret += ','
-		ret = ret + name + '=' + "'" + value + "'"
+		ret = ret + str(name) + '=' + "'" + str(value) + "'"
 	return ret
 
-def print_calls():
-	for call in calls:
-		ret = eval(call)
-		temp = call + "=" + repr(ret)	
-		open('print_calls.txt','a').write(temp + '\n')		
+def print_calls(func_name,index):
+	global op_file
+	if not op_file:
+		op_file = open('print_calls.txt','w')
+	temp = calls[func_name]
+	stack_f = temp["stack_call"][index]
+	module_name = print_leaf(temp['module_name'])	
+	res = module_name + ':' + func_name + ':' + stack_f['args'] + ':' + str(stack_f['ret'])
+	op_file.write(res + '\n')
